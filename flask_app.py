300e1d75b66db333fb5faa121c663fc93139c340
vldplcd <shilov.part@gmail.com>
2021-08-22 15:05:59 +0300

    auth

--- flask_app.py
+++ flask_app.py
@@ -1,6 +1,7 @@
-from flask import Flask, jsonify, request,render_template
+from flask import Flask, jsonify, request, render_template, session, redirect
 from flask_cors import CORS, cross_origin
-import json
+import requests
+import urllib
 
 import random
 
@@ -8,9 +9,6 @@ import json
 from datetime import datetime
 import time
 import ssl
-import pymongo
-from urllib.parse import quote_plus as quote
-from bson.objectid import ObjectId
 import base64
 import secrets
 import pandas as pd
@@ -18,16 +16,22 @@ import json
 import plotly
 import plotly.express as px
 
+client_id = '0a5c1ff2ba7e4bdd83ee228720efacb5'
+client_secret = 'ab444188a16e471cbbdd48965449dff3'
 
 
 def create_app(app_name='YAMOOD_API'):
     app = Flask(app_name)
+    app.secret_key = 'rand'+str(random.random())
     CORS(app, resources={r"/api/*": {"origins": "*"}})
-
 
     @app.route('/')
     def main_page():
-        return 'Tell me you are sad without telling me you are sad'
+        if 'access_token' in session:
+            return f'Tell me you are sad without telling me you are sad <br> {session["access_token"]}'
+        else:
+
+            return render_template('login.html', client_id=client_id)
 
     @app.route('/dash_test',methods=['GET', 'POST'])
     def notdash():
@@ -40,7 +44,6 @@ def create_app(app_name='YAMOOD_API'):
         graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
         return render_template('notdash.html', graphJSON=graphJSON)
 
-
     @app.route('/api', methods=['GET'])
     @cross_origin()
     def default_f():
@@ -50,8 +53,38 @@ def create_app(app_name='YAMOOD_API'):
                     'statusCode': 400
                 }), 400
 
+    def get_token(code):
+        token_auth_uri = f"https://oauth.yandex.ru/token"
+        headers = {
+            'Content-type': 'application/x-www-form-urlencoded',
+        }
+        query = {
+            'grant_type': 'authorization_code',
+            'code': code,
+            'client_id': client_id,
+            'client_secret': client_secret,
+        }
+        query = urllib.parse.urlencode(query)
+
+        resp = requests.post(token_auth_uri, data=query, headers=headers)
+        print(resp.text)
+        rj = resp.json()
+        return rj['access_token']
+
+    @app.route('/auth', methods=['GET'])
+    @cross_origin()
+    def auth():
+        if request.method == "GET":
+            code = request.args.get('code')
+            token = get_token(code)
+            session['access_token'] = token
+            return redirect('/')
+        return jsonify({
+                    'statusCode': 400
+                }), 400
 
     return app
+
 
 if __name__ == "__main__":
     app = create_app()
